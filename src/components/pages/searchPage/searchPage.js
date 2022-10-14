aa5f463f9df74e3ed20025301097bd820c285988
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2022-10-14 20:57:13 +0300

    add route

---
+++ src/components/pages/searchPage/searchPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function SearchPage(props) {
+    return <h1>Search by filters</h1>
+}
+
+export default SearchPage;
--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -4,6 +4,7 @@ import ForBuyPage from "../pages/forBuyPage";
 import ForRentPage from "../pages/forRentPage";
 import HomePage from "../pages/homePage";
 import ItemDetailPage from "../pages/itemDetailPage/itemDetailPage";
+import SearchPage from "../pages/searchPage/searchPage";
 
 const  Routers = ()  => {
     return (
@@ -11,6 +12,7 @@ const  Routers = ()  => {
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path={'/home'} element={<HomePage/>}/>
             <Route path={'/home/:purpose/:externalID'} element={<ItemDetailPage/>}/>
+            <Route path = {'/search'} element={<SearchPage/>}/>
             <Route path = "/sale" element={<ForBuyPage/>}/>
             <Route path = "/rent" element={<ForRentPage/>}/>
             <Route path = 'rent/:externalID' element={<ItemDetailPage/>}/>
