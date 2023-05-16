5276d5be2f1abd2b570602b46502988b98076965
Artem Tyutyunik <artem.tyutyunik03@gmail.com>
2023-05-17 01:09:26 +0300

    fix visual bugs on small sizes

--- src/components/carousels/homePageCarousel/homePageCarousel.js
+++ src/components/carousels/homePageCarousel/homePageCarousel.js
@@ -1,24 +1,28 @@
-import "./slick-theme.css";
-import './carousel-slick.css';
 import Slider from "react-slick";
-import RecommendCard from "../../recommendCard/recommend card";
 import {Link as BrowserLink} from 'react-router-dom'
+import RecommendCard from "../../recommendCard/recommend card";
+import useWindowSize from "../../../hooks/useWindowSize";
+import './carousel-slick.css';
+import "./slick-theme.css";
+
 
 export default function HomePageCarousel(props)  {
     const [renderData] = props.data
+    const {width} = useWindowSize()
 
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        arrows: width > 800,
+        slidesToShow: width > 800 ? 3: width > 650? 2: 1,
+        slidesToScroll: width > 700 ? 3: 1
     };
 
     const filteredRenderData = renderData?.hits.filter(item => item.product === 'hot')
 
     return (
-        <div>
+        <div className={'inner_slick'}>
             <h2 className="title">{props.title}</h2>
             <Slider {...settings}>
                 {
